c4cc5112066c8ee72d942238a96aabf6a259959f
clara <10088152+claraj@users.noreply.github.com>
2020-09-26 18:04:42 -0500

    settings, debug off

--- movieapi/settings.py
+++ movieapi/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(g1on48t(-*^ad=)2bvpg0d7fky%xp#k1#pz++@y)*-8u6zs2)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -136,5 +135,7 @@ STATIC_ROOT = 'static'
 
 # Rest framework auth config
 
+# Django database settings 
 
+import django_heroku
 django_heroku.settings(locals())
